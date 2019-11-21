a462426b3bcdc9fb4358976725985f21d99912fc
Christian Gawron <gawron.christian@fh-swf.de>
2019-11-21 17:32:36 +0100

    added scripts

---
+++ scripts/receive_and_process_satellite.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# $1 = Satellite Name
+# $2 = Frequency
+# $3 = FileName base
+# $4 = TLE File
+# $5 = EPOC start time
+# $6 = Time to capture
+
+sudo timeout $6 rtl_fm -f ${2}M -s 48k -g 45 -p 16 -r 48 -E deemp -F 9 - | sox -t raw -r 48k -e s -b 16 -c 1 -V1 - $3.wav rate 11025
+
+PassStart=`expr $5 + 90`
+
+if [ -e $3.wav ]
+  then
+    /usr/local/bin/wxmap -T "${1}" -H $4 -p 0 -l 0 -o $PassStart ${3}-map.png
+
+    /usr/local/bin/wxtoimg -m ${3}-map.png -e ZA $3.wav $3.png
+fi
