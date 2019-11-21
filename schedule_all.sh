8d5ba68a4ebf3dec19b66c0e4442195b57ae20a3
Christian Gawron <gawron.christian@fh-swf.de>
2019-11-21 16:31:46 +0100

    Update schedule_all.sh

--- schedule_all.sh
+++ schedule_all.sh
@@ -10,6 +10,6 @@ grep "NOAA 19" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/weather/pre
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 
 #Schedule Satellite Passes:
-/home/pi/weather/predict/schedule\_satellite.sh "NOAA 19" 137.1000
-/home/pi/weather/predict/schedule\_satellite.sh "NOAA 18" 137.9125
-/home/pi/weather/predict/schedule\_satellite.sh "NOAA 15" 137.6200
+/home/pi/weather/predict/schedule_satellite.sh "NOAA 19" 137.1000
+/home/pi/weather/predict/schedule_satellite.sh "NOAA 18" 137.9125
+/home/pi/weather/predict/schedule_satellite.sh "NOAA 15" 137.6200
